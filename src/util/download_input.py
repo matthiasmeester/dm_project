784025a136edf83b5fa3f39d8e85660905e76828
mmeester <matthiasmeester@gmail.com>
2022-01-20 09:43:37 +0100

    Setup project structure + reformat  ipynb file

---
+++ src/util/download_input.py
@@ -0,0 +1,9 @@
+from src.util.toespraken import download_toespraken
+
+
+def download_input():
+    download_toespraken()
+
+
+if __name__ == "__main__":
+    download_input()
---
+++ src/util/toespraken.py
@@ -0,0 +1,32 @@
+import pathlib
+
+import pandas as pd
+import requests
+from bs4 import BeautifulSoup
+
+PERSCONFERENTIES_API_URL = 'https://www.rijksoverheid.nl/onderwerpen/coronavirus-covid-19/coronavirus-beeld-en-video/videos-persconferenties'
+
+
+def download_toespraken():
+    response = requests.get(PERSCONFERENTIES_API_URL)
+    soup = BeautifulSoup(response.content, "html.parser")
+
+    urls = []
+    for page_content in soup.findAll('a', href=True):
+        urls.append(page_content['href'])
+    urls = pd.Series(urls)
+    urls = urls[urls.str.contains('letterlijk')]
+    urls = 'http://www.rijksoverheid.nl' + urls
+
+    pathlib.Path('../input/toespraken/').mkdir(parents=True, exist_ok=True)
+    for i, url in enumerate(urls):
+        response_text = requests.get(url)
+        soup = BeautifulSoup(response_text.content, "html.parser")
+        texts = [p.get_text() for p in soup.find_all('p')]
+        file_name = f"../input/toespraken/toespraak_{i}.txt"
+        with open(file_name, "w", encoding='utf-8') as txt_file:
+            txt_file.write('\n'.join(texts))
+
+
+def preprocess():
+    pass
